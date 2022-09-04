282c37d445ba6a5f9e1759addcf6f0a5172a9ca3
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2022-09-04 09:47:23 +0530

    action created

---
+++ src/components/CakeContainer.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function CakeCountainer() {
+  return (
+    <div>
+      <h1>Number of Cakes</h1>
+      <button>Buy Cake</button>
+    </div>
+  );
+}
