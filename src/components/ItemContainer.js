4728dbd4012c0cb43ec9d517ae3c91a2c50e69c9
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2022-09-04 17:27:58 +0530

    about own props in mapDispatchToProps

--- src/components/ItemContainer.js
+++ src/components/ItemContainer.js
@@ -1,9 +1,12 @@
 import React from "react";
 import { connect } from "react-redux";
+import { buyCake, buyIceCream } from "../redux";
+
 function ItemContainer(props) {
   return (
     <div>
       <h2>Item - {props.item}</h2>
+      <button onClick={props.buyItem}>Buy Item</button>
     </div>
   );
 }
@@ -13,4 +16,11 @@ const mapStateToProps = (state, ownProps) => {
   return { item: itemState };
 };
 
-export default connect(mapStateToProps)(ItemContainer);
+const mapDispatchToProps = (dispatch, ownProps) => {
+  const dispatchFunction = ownProps.cake ? () => dispatch(buyCake()) : () => dispatch(buyIceCream());
+  return {
+    buyItem: dispatchFunction,
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ItemContainer);
